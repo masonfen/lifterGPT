061fd8eff573757e7fa03387711e3e370176fb23
Headyman <ma5onfn1@gmail.com>
2024-07-19 20:48:13 -0700

    added every button and such

--- src/App.js
+++ src/App.js
@@ -5,29 +5,57 @@ function App() {
   return (
     <div className="App">
     <div className="title-container">
-      <Title /> <Second_Title />
+      <LifterTitle /> <GPT_Title />
     </div>
+
+{/* buttons for assigning gender roles */}
+    <div className= "Gender-container">
+    <Button label = "Male lifter" onClick={() => alert('Male clicked!')} />
+    <Button label = "Female lifter" onClick={() => alert('Female clicked')}/>
+    </div>
+
+{/* buttons for assigning goal type */}
     <div className= "GoalType-container">
-    <Button label= "Hypertrophy" onClick={() => alert('Button clicked!')} />
+    <Button label= "Hypertrophy" onClick={() => alert('Hypertrophy clicked!')} />
+    <Button label= "Strength" onClick={() => alert('Strength clicked!')} />
     </div>
+
+{/* assigning workout type */}
+    <div className= "WeightSelection-container">
+      <WeightSelection />
     </div>
+
+    <div className= "GO-container">
+      <Button label = "GO" onClick={() => alert('GO clicked!')} />
+    </div>
+
+    </div>
+
+
+
   );
 }
 
-//title for the app
-function Title() {
+
+
+
+
+                                           // *****COMPONENTS*****
+
+//titles for the app
+function LifterTitle() {
   return (
-    <h1 className ="title">Lifter</h1>
+    <h1 className ="lifter_title">Lifter</h1>
   );
 }
 
-function Second_Title() {
+function GPT_Title() {
   return (
-    <h1 className = "second_title">GPT</h1>
+    <h1 className = "gpt_title">GPT</h1>
   );
 }
 
-//button for hypertrophy or strength
+//button component
 function Button({ label, onClick }) {
   return (
     <button className="custom-button" onClick={onClick}>
@@ -37,3 +65,15 @@ function Button({ label, onClick }) {
 }
 
 export default App;
+
+
+function WeightSelection() {
+  return (
+    <form>
+      <label>
+        Weight:
+        <input type="text" name="name" />
+      </label> 
+    </form>
+  )
+}
